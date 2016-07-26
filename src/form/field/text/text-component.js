ebf6d656b9b280249a4e3ac99a89479f182370d4
Hai Huynhlam <hhuynhlam@gmail.com>
2016-07-25 17:53:42 -0700

    chore(*): eject react-scripts for more flexibility
    Eject React-Scripts for more flexibility and add hot module reload for
    React components.

--- src/form/field/text/text-component.js
+++ src/form/field/text/text-component.js
@@ -4,6 +4,7 @@ class Text extends React.Component {
   render() {
     return (
       <input type="text"
+             style={{borderColor: 'red'}}
              {...this.props}>
       </input>
     );
